c0e57c7703ac625b1ddf02dcf0a02d2acf160a7d
Ashwini-Anand01 <anandrkt120@gmail.com>
2025-06-11 10:54:53 +0530

    total wage is calcauated

---
+++ UC-5.js
@@ -0,0 +1,78 @@
+function Check_Employee(){
+    let attendance=Math.floor(Math.random()*2);
+    let present=0
+    if(attendance==1){
+        present=1;
+    }
+    return present;
+}
+let present=Check_Employee();
+ console.log(present);
+//uc-2
+function worktimes(){
+    let time=Math.floor(Math.random()*3);
+    let workertime=0
+    if(time==1){
+        workertime=1;
+    }
+    else if(time==2){
+        workertime=2;
+    }
+    return workertime;
+}
+//uc-3
+function DailyWorkHours(){
+    let WorkerTypes=worktimes();
+    let hour=0
+    switch (WorkerTypes) {
+        case 0:
+            hour=0
+            break;
+        case 1:
+            hour=4
+            break;
+        case 2:
+            hour=8
+            break;
+    }
+    return hour
+}
+//uc-4
+let montly_wages=0;
+let montly_hours=0;
+let hours=0;
+let daily_wages=0
+hours=DailyWorkHours();
+daily_wages=hours*20;
+montly_hours=hours*20;
+montly_wages=daily_wages*20;
+console.log(`Daily Hours of the Employee is: ${hours}`);
+console.log(`Daily Wages: ${daily_wages}`);
+console.log(`Montly Hours: ${montly_hours}`);
+console.log(`Montly Wages: ${montly_wages}`);
+//uc-5
+hours = 0;
+let max_days = 0;
+let max_wage = 0;
+while (hours <= 160 && max_days < 20) {
+    const workerTypes = worktimes();
+    switch (workerTypes) {
+        case 1:
+            max_days++;
+            break;
+        case 0:
+            hours += 4;
+            max_days++;
+            max_wage += 20 * 4;
+            break;
+        case 2:
+            hours += 8;
+            max_days++;
+            max_wage += 20 * 8;
+            break;
+    }
+}
+
+console.log(`Total Working Hours: ${hours}`);
+console.log(`Total Working Days: ${max_days}`);
+console.log(`Total Wage for the Month: $${max_wage}`);
